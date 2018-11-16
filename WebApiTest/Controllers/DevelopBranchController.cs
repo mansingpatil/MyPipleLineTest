4d4c86ec468cb68f957e233292472109a9f9d346
Mansing <mansing.patil@acando.no>
2018-11-16 10:17:08 +0100

    Comment

---
+++ WebApiTest/Controllers/DevelopBranchController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevelopBranchController : ControllerBase
+    {
+        // GET: api/DevelopBranch
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "DevelopBranch1", "DevelopBranch2" };
+        }
+
+        // GET: api/DevelopBranch/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "DevelopBranch1";
+        }
+
+   
+    }
+}
